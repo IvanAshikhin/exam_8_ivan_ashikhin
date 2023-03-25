a757833ab6bb4daf0a4bfc41e5301d67ca465969
Ivan <ashikhin1998@gmail.com>
2023-03-25 16:29:50 +0600

    Регистрация, логин, логаут
    Реализовал регистрацию пользователя, вход и выход пользователя

--- exam_8/settings.py
+++ exam_8/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'exam_8',
-    'product'
+    'product',
+    'accounts'
 ]
 
 MIDDLEWARE = [
