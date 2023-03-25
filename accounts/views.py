15b2227a7400bbe093a9c78c78cc60482ff6a296
Ivan <ashikhin1998@gmail.com>
2023-03-25 17:56:14 +0600

    Смена био и пароля
    Реализовал смену био и пароля пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.shortcuts import redirect
-from django.views.generic import TemplateView, CreateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, UserChangeForm, PasswordChangeForm
 
 from accounts.forms import CustomUserCreationForm
+from product.models import Review
 
 
 class LoginView(TemplateView):
@@ -50,5 +53,46 @@ class RegisterView(CreateView):
         return self.render_to_response(context)
 
 
-class AccountDetailView()
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        reviews = Review.objects.all().filter(author=self.object.pk)
+        kwargs['reviews'] = reviews
+        return super().get_context_data(**kwargs)
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+    success_url = reverse_lazy('index_page')
 
